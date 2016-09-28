b3180a82b9793671a4f533bee07aa7493112ab1b
hsenidVidushka <vidushka@hsenidlanka.com>
2016-09-28 00:56:11 -0500

    class branching edited

--- src/main/java/com/hsenid/branching/branching.java
+++ src/main/java/com/hsenid/branching/branching.java
@@ -4,4 +4,21 @@ package src.main.java.com.hsenid.branching;
  * Created by hsenid on 9/28/16.
  */
 public class branching {
+    public static void main(String[] args) {
+        for (int i = -5; i <= 5; i++) {
+            int k;
+            if (i < 0)
+                k = -i;
+            else
+                k = i;
+            int l = 5 - k;
+            for (int j = -5; j <= 5 + k; j++) {
+                if (j < l)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            System.out.println("--------------------------");
+        }
+    }
 }
