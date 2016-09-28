05ee964a2bc90683277ddc1276a1b8847aa1e801
hsenidVidushka <vidushka@hsenidlanka.com>
2016-09-28 01:11:27 -0500

    last edit

--- src/main/java/com/hsenid/branching/branching.java
+++ src/main/java/com/hsenid/branching/branching.java
@@ -18,7 +18,7 @@ public class branching {
                 else
                     System.out.print("*");
             }
-            System.out.println("------------------------");
+            System.out.println("-----------------------");
         }
     }
 }
